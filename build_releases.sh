e113e86243bdf575933e0ea1ab873c04269ee77d
Jeremy McAnally <jeremymcanally@gmail.com>
2022-05-28 12:06:41 -0400

    Add .msi building to release build script

--- build_releases.sh
+++ build_releases.sh
@@ -3,16 +3,30 @@
 rm -rf ./build
 rm -rf ./releases
 
+echo "Building README HTML file..."
+mkdir build
+cmark-gfm README.md > build/README.html
+
 platforms=("darwin" "darwin" "linux" "linux" "linux" "windows" "windows")
 archs=("arm64" "amd64" "386" "arm64" "amd64" "386" "amd64")
 
 mkdir -p releases
 tbVersion=`grep ToyboxVersion version.go | head -n 1 | cut -d '"' -f2`
 
+echo "Cross-building version ${tbVersion}..."
 for ((i=0;i<${#platforms[@]};i++))
 do
 	mkdir -p build/${platforms[$i]}-${archs[$i]};
 	GO111MODULE=off GOOS=${platforms[$i]} GOARCH=${archs[$i]} go build -o build/${platforms[$i]}-${archs[$i]}/toybox
-	cp README.md build/${platforms[$i]}-${archs[$i]}/
+	cp build/README.html build/${platforms[$i]}-${archs[$i]}/
+
+	if [ ${platforms[$i]} = 'windows' ]
+	then
+		mv build/${platforms[$i]}-${archs[$i]}/toybox build/${platforms[$i]}-${archs[$i]}/toybox.exe
+	fi
+
 	zip -r releases/toybox-${tbVersion}-${platforms[$i]}-${archs[$i]}.zip build/${platforms[$i]}-${archs[$i]}/*
 done
+
+echo "Building .msi..."
+wixl -v deploy/toybox.wxs -o releases/toybox.msi
